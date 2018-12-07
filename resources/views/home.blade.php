f4c716319624684eb23a3c34946b3d366afbc677
KyleWardle <kylewardle@bspokeit.co.uk>
2018-12-07 12:55:43 +0000

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-sm-12">
+
+                <div class="card">
+                    <div class="card-body text-center">
+                        <h1>Quiz Manager</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Options</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <a href="" class="btn btn-info">Create New Quiz</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Current Quizes</h3>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
